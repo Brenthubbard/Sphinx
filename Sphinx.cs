597fb89b876fbcba1237124c91e3c747595c9226
Brenthubbard <76464105+Brenthubbard@users.noreply.github.com>
2021-05-03 16:00:29 -0600

    finished build and update repo
    Co-authored-by: Seth Medeiros <Medeirosseth@gmail.com>

--- Sphinx.cs
+++ Sphinx.cs
@@ -31,14 +31,35 @@ class Sphinx
   }
 static void  PlayGame1()
 {
-  Console.WriteLine("Your next questions 'Im tall when Im young and short when im old........what am I?'");
+  Console.WriteLine("Your next question is 'Im tall when Im young and short when im old........what am I?'");
   string answer = Console.ReadLine();
   if (answer == "A Candle" || answer == "a candle" || answer == "candle")
   {
-    Console.WriteLine("You are correct but you must finish the game or PERISH in the acids of my ancient stomach"),
+    Console.WriteLine("You are correct but you must finish the game or PERISH in the acids of my ancient stomach!");
+    PlayGame2();
+  }
+  else{
+    Console.WriteLine("Your toast! GET IN MY BELLY. Do you want to just hop in or do you prefer me to chew you up first????");
+  }
+}
+static void PlayGame2()
+{
+  Console.WriteLine("What has to be broken before you can use it?");
+  string answer = Console.ReadLine();
+  if (answer == "An Egg"  ||  answer == "an Egg" || answer == "an egg" || answer == "egg" || answer == "Egg")
+  {
+    Console.WriteLine(" You have Answered all of the questions correctly, you may move forward to the next challenge.....");
+    Console.WriteLine("Or you can try and out match me again IF YOU DARE!!!! press the Y key to try again");
+    string answerToProceed = Console.ReadLine();
+    if(answerToProceed == "Y" || answerToProceed == "y")
+    {
+    PlayGame();
+    }
+  }
+  else
+  {
+    Console.WriteLine("Your toast! GET IN MY BELLY. Do you want to just hop in or do you prefer me to chew you up first????");
+    
   }
 }
 }
-// PlayGame1(); 
-// PlayGame2(); 
-// PlayGame3(); 
